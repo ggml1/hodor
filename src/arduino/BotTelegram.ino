148793f89046d429546bd2122c59d624e70e9f52
Douglas Pontes <jdps@cin.ufpe.br>
2019-11-26 19:57:30 -0300

    Refactoring

--- src/arduino/BotTelegram.ino
+++ src/arduino/BotTelegram.ino
@@ -5,21 +5,46 @@
 #include <UniversalTelegramBot.h>
 #include <Servo.h>
 
-Servo myservo;
- 
+
+class Gate{
+public:
+	Gate(int pin);
+	void open();
+	void close();
+private:
+	Servo atuador;
+}
+
+Gate::Gate(int pin){
+	atuador.attach(pin);
+}
+
+void Gate::open(){
+  	atuador.write(180);
+}
+
+void Gate::close(){
+	atuador.write(90);
+}
+
 // Initialize Wifi connection to the router
 char ssid[] = SSID;     // your network SSID (name)
 char password[] = PASSWORD; // your network key
  
 // Initialize Telegram BOT
 #define BOTtoken TOKEN  // your Bot Token (Get from Botfather)
- 
+
+#define GATE_PIN 13
+
+Gate gate(GATE_PIN);
+
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
  
 int Bot_mtbs = 1000; //mean time between scan messages
 long Bot_lasttime;   //last time messages' scan has been done
  
+
 void setup() {
   Serial.begin(115200);
  
@@ -36,7 +61,7 @@ void setup() {
     Serial.print(".");
     delay(500);
   }
-  myservo.attach(13);
+
 }
  
 void loop() {
@@ -48,15 +73,15 @@ void loop() {
       
       for (int i=0; i<numNewMessages; i++) {
         if(bot.messages[i].text == "abre"){
-          Serial.println("abrindo");
-                  bot.sendMessage(bot.messages[i].chat_id, "abrindo", "");
 
-          myservo.write(180);
+          bot.sendMessage(bot.messages[i].chat_id, "abrindo", "");
+		  gate.open();
+
         }else{
-          Serial.println("fechando");
-                  bot.sendMessage(bot.messages[i].chat_id, "fechando", "");
 
-          myservo.write(90);
+          bot.sendMessage(bot.messages[i].chat_id, "fechando", "");
+		  gate.close();
+
         }
       }
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
